f76deab24933babc544b499ae26bff08e14e4bc4
setkarosetka-hue <setkarosetka@gmail.com>
2026-09-22 19:51:27 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://spszgaoxonrqvtebssdo.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_7BtmLm6E6Q0wjoLGUMb_Ag_udvfth7K";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
